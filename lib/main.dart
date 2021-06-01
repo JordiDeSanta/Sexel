6e96f84d1240d45237d14ebe8db2fff5dacc828e
Jordi <jordi.bravo.bad@gmail.com>
2021-05-31 20:27:21 -0400

    Fonts import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sexel/src/pages/home_page.dart';
 
 void main() {
@@ -9,12 +10,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarDividerColor: Colors.transparent,
+      ),
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Sexel',
       theme: ThemeData(
         primarySwatch: Colors.red,
         primaryColor: Colors.red.shade200,
+        primaryTextTheme: TextTheme(
+          headline6: TextStyle(
+            color: Colors.white,
+            fontSize: 50,
+            fontFamily: 'Sweet',
+          ),
+          caption: TextStyle(
+            color: Colors.black,
+            fontSize: 12,
+            fontFamily: 'Coolvetica',
+          ),
+        ),
       ),
       initialRoute: 'home',
       routes: {
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,13 +1,22 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double h = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Sexel'),
         elevation: 0.0,
         centerTitle: true,
+        toolbarHeight: h * 0.1,
+      ),
+      body: Text(
+        'Wena po ctm',
+        style: Theme.of(context).textTheme.caption,
       ),
     );
   }
