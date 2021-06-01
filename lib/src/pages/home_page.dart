bcd7042adcd27c69dbc60419e571c2addd79eabf
Jordi <jordi.bravo.bad@gmail.com>
2021-05-31 20:02:31 -0400

    Home Page init

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sexel'),
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+    );
+  }
+}
